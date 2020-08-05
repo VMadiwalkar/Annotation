be17adf6baca310f65e671a3250d969a994932eb
Rikesh Subedi <rikesh.subedi@sap.com>
2020-08-05 09:34:34 +0530

    eraser fixes

--- app/src/main/java/com/example/annotation/MainActivity.kt
+++ app/src/main/java/com/example/annotation/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.annotation
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.SeekBar
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,24 +22,47 @@ class MainActivity : AppCompatActivity() {
 
 
         setContentView(R.layout.activity_main)
+        val strokeWidth = 40
+        drawing?.setStrokWidth(strokeWidth.toFloat())
+        strokeWidthSelector?.progress = strokeWidth
         btnGreen.setOnClickListener(){
             Log.d("Main","Red Clicked");
-            drawing.changeStroke(Color.GREEN)
+            drawing.setPencilColor(Color.GREEN)
 
 
         }
         btnErase.setOnClickListener(){
-            drawing.clear()
+            drawing.setMode(SimpleDrawingView.DrawingMode.ERASER)
         }
 
         btnRed.setOnClickListener(){
-            drawing.changeStroke(Color.RED)
+            drawing.setPencilColor(Color.RED)
         }
 
-       /*val bitmap: Bitmap = Bitmap.createBitmap(500,500,Bitmap.Config.ARGB_8888)
-        val v: View = Annotate(applicationContext)
-        val canvas:Canvas = Canvas(bitmap)
-        v.draw(canvas)
-        image.setImageBitmap(bitmap)*/
+        strokeWidthSelector.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                drawing?.setStrokWidth(progress.toFloat())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
     }
+
+    fun setEraseMode(view: View) {
+        drawing?.setMode(SimpleDrawingView.DrawingMode.ERASER)
+    }
+
+    fun setPencilColor(view: View) {
+        val color = (view.background as? ColorDrawable)?.color ?: Color.BLACK
+        drawing?.setMode(SimpleDrawingView.DrawingMode.ERASER)
+        drawing?.setPencilColor(color)
+    }
+
+
 }
